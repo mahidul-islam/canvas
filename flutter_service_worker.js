d4a5402b33e670643239cffe625d1d3661a3777d
mahidul-islam <mizihan84@gmail.com>
2021-08-21 15:52:15 +0600

    Built web
    Branch: master
    Commit: 1a78c5bfb6da8a4f438d646c4c39f72f58362646
    
    package:peanut 4.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "1a1f1cac22a7bd747645ba138222477e",
-"index.html": "4d398c3a217c32c0e4b156eb8f660f17",
-"/": "4d398c3a217c32c0e4b156eb8f660f17",
-"main.dart.js": "e4ef981f34e109d68bb4fe7c6dc986eb",
+"index.html": "90952e5b3fd8afffe85403e3ca48ed82",
+"/": "90952e5b3fd8afffe85403e3ca48ed82",
+"main.dart.js": "9dc27ce3c5fd205cf0bb183c3a875eb9",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
