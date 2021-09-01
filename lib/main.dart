6496ce09ea6f51916cbbf2a3c1aae2f7dc19b684
mahidul-islam <mizihan84@gmail.com>
2021-09-01 12:35:48 +0600

    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Draggable Custom Painter',
+      title: 'Custom Painter Experiments',
       home: Scaffold(
         body: PagerWidget(),
       ),
