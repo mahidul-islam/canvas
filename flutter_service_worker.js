3f3acefbb4f6902b6867dc153e37f558e2d8faa3
mahidul-islam <mizihan84@gmail.com>
2021-10-14 10:53:13 +0600

    Built web
    Branch: master
    Commit: 07c1914e8f7a8a340e065f3c1511d60c62ef9349
    
    package:peanut 4.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,18 +4,19 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "1a1f1cac22a7bd747645ba138222477e",
-"index.html": "731cbe4257370c810e8b08be4173fdd2",
-"/": "731cbe4257370c810e8b08be4173fdd2",
-"main.dart.js": "cb8842858cc3ab0850eb3ccb2b72e68d",
+"index.html": "4a3a52a3d996aea68a0ddea9e213ba47",
+"/": "4a3a52a3d996aea68a0ddea9e213ba47",
+"main.dart.js": "2cc9468df1cf62c894cce617b6d34e96",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "1e99c19602fb2107ee48e8de0a43ccc7",
-"assets/AssetManifest.json": "795eca3eab9bdfa574b4be43200cd324",
-"assets/NOTICES": "4de6d012807be7fab68a72fe58f32869",
+"assets/AssetManifest.json": "69f56513db8d3948ca55d31f09bc34be",
+"assets/NOTICES": "513f1afb5d21cd092fffd0c29cd63a85",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/assets/Nutrition.md": "ee8245ac4126d72e9a5562a378631732",
 "assets/assets/img.png": "19787b358ba6761e60f9396025260a0b"
 };
 
